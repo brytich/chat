489e32f426945ec8be45247e16d35fc215e3103e
unknown <bryan.catiche@gmail.com>
2021-06-01 18:03:20 +0200

    final(bug cross a voir et a corriger)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import Login from './Login';
+import React from 'react'
+import Login from './Login'
 import useLocalStorage from '../hooks/useLocalStorage';
-import Dashboard from './Dashboard';
+import Dashboard from './Dashboard'
+import { ContactsProvider } from '../contexts/ContactsProvider'
+import { ConversationsProvider } from '../contexts/ConversationsProvider';
+import { SocketProvider } from '../contexts/SocketProvider';
 
 function App() {
   const [id, setId] = useLocalStorage('id')
-return (
 
-  id ? <Dashboard/> : <Login onIdSubmit={setId} /> 
+  const dashboard = (
+    <SocketProvider id={id}>
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Dashboard id={id} />
+        </ConversationsProvider>
+      </ContactsProvider>
+    </SocketProvider>
+  )
 
-
-)
+  return (
+    id ? dashboard : <Login onIdSubmit={setId} />
+  )
 }
-
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,37 +1,30 @@
-//Import
-import React, { useRef} from 'react'
-import { Container, Form ,Button } from 'react-bootstrap'
+import React, { useRef } from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
 import { v4 as uuidV4 } from 'uuid'
 
-//function
-//1-> A chaque click sur le button login , on appelle la function(handlesubmit) paramettré pour envoyer le form vers.... 
-export default function Login({onIdSubmit}) {
-    const idRef = useRef()
+export default function Login({ onIdSubmit }) {
+  const idRef = useRef()
 
-    function handleSubmit(hand) {
-        hand.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault()
 
-        onIdSubmit(idRef.current.value)
+    onIdSubmit(idRef.current.value)
+  }
 
-    }
-//2-> A chaque click sur le button New ID on appelle la function createNewId , qui creera un id perso a l user grace a UUIV4 (nmp i uuid)
-    function createNewId(clickb) {
-        onIdSubmit(uuidV4())
-    }
+  function createNewId() {
+    onIdSubmit(uuidV4())
+  }
 
-
-//Jsx
-    return (
-        <Container className="align-items-center d-flex" style= {{ height: '100vh'}}>
-            <Form onSubmit={handleSubmit} className="w-100">
-                <Form.Group>
-                    <Form.Label>Entre l'Id</Form.Label>
-                    <Form.Control placeholder="tape un truc" type="text" ref={idRef} required/>
-                </Form.Group>
-                <Button type="submit" className="mr-2">Login</Button>
-                <Button onClick={createNewId} variant="secondary" >New Id</Button>
-            </Form>
-        </Container>
-        
-    )
+  return (
+    <Container className="align-items-center d-flex" style={{ height: '100vh' }}>
+      <Form onSubmit={handleSubmit} className="w-100">
+        <Form.Group>
+          <Form.Label>Enter Your Id</Form.Label>
+          <Form.Control type="text" ref={idRef} required />
+        </Form.Group>   
+        <Button type="submit" className="mr-2">Login</Button>
+        <Button onClick={createNewId} variant="secondary">Create A New Id</Button>
+      </Form>
+    </Container>
+  )
 }
